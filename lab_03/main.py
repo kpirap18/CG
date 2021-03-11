662e74222134ba4d8a713a6dfc75e92d4df4127d
kpirap18 <ir1nak0z@yandex.ru>
2021-03-11 21:04:26 +0300

    Add new v

--- lab_03/main.py
+++ lab_03/main.py
@@ -5,6 +5,8 @@ import pyqtgraph as pg
 import matplotlib.pyplot as plt
 
 from time import time
+from for_time import *
+from for_time2 import *
 from copy import deepcopy
 from PyQt5 import QtWidgets, QtCore, QtGui
 from math import sin, cos, pi, radians, fabs,  floor
@@ -14,6 +16,7 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
         self.graphicsView.scale(1, 1)
         self.scene = QtWidgets.QGraphicsScene()
         self.graphicsView.setScene(self.scene)
@@ -42,7 +45,7 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
         self.pushButton_line_one.clicked.connect(self.draw_line)
         self.pushButton_step_all.clicked.connect(self.com_step)
         self.pushButton_step_ch.clicked.connect(self.com_step_one)
-        self.pushButton_time_all.clicked.connect(self.com_time)
+        self.pushButton_time_all.clicked.connect(self.time_all)
         self.pushButtonDrawSpectrum.clicked.connect(self.draw_spectr)
 
         self.action.triggered.connect(self.about)
@@ -69,6 +72,38 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
                         'Анализ ступенчатости и времени исполнения приводится\n'
                         'в виде графиков pyplot.\n')
         return
+
+    def time_all(self):
+        lenn, angle = self.read_ang_line()
+        if lenn is None or angle is None:
+            return
+        
+        h = self.graphicsView.height()
+        w = self.graphicsView.width()
+        self.radioButtonLib.toggle()
+        start = time()
+        for _ in range(100):
+            teta = 0
+            while teta < 2 * pi - 0.001:
+                x = w // 2 + lenn * cos(teta)
+                y = h // 2 + lenn * sin(teta)
+                self.scene.addLine(w // 2, h // 2, x, y)
+                teta += radians(angle)
+        t_lib = ((time() - start) / 100)
+        print(t_lib)
+        timeResearch(lenn, angle, t_lib)
+        self.scene.clear()
+
+    def read_ang_line(self):
+        try:
+            angle = float(self.lineEditAngle.text())
+            lenn = float(self.lineEdiLen.text())
+        except:
+            lenn = 200
+            angle = 10
+
+        return lenn, angle
+
     def clean_screen(self):
         self.scene.clear()
 
@@ -104,33 +139,26 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
         self.graphicsView.setStyleSheet("background-color: red")
 
     def set_green_bg(self):
-        self.graphicsView.setStyleSheet("background-color: green")
+        self.graphicsView.setStyleSheet("background-color: #00ff00")
 
     def set_yellow_bg(self):
         self.graphicsView.setStyleSheet("background-color: yellow")
 
 
     def my_addline(self, p_start, p_end, draw=True):
-        # print("draw in my_addline", draw)
-        # print(self.radioButtonCDA.isChecked())
         if self.radioButtonCDA.isChecked():
-            # print("tyt1")
             self.dda(p_start, p_end, draw=draw)
         elif self.radioButtonBresFloat.isChecked():
-            # print("tyt2")
             self.bresen_float(p_start, p_end, draw=draw)
         elif self.radioButtonBresInt.isChecked():
-            # print("tyt3")
             self.bresen_int(p_start, p_end, draw=draw)
         elif self.radioButtonBresSmooth.isChecked():
-            # print("tyt4")
             self.bresen_smooth(p_start, p_end, draw=draw)
         elif self.radioButtonWu.isChecked():
-            # print("tyt5")
             self.wu(p_start, p_end, draw=draw)
         else:
-            # print("tyt6")
             self.lib(p_start, p_end)
+
 
     def draw_point(self, x, y, light=255):
         color = self.pen.color()
@@ -138,33 +166,30 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
         self.pen.setColor(color)
         self.scene.addLine(x, y, x, y, self.pen)
     
-###############################################
+
     def draw_spectr(self, buf=False, draw=True):
-        # print("draw_spectr", draw)
-        # self.scene.addLine(0, 0, 200, 200)
         try:
-            angle = int(self.lineEditAngle.text())
-            lenn = int(self.lineEdiLen.text())
-        except TypeError:
+            angle = float(self.lineEditAngle.text())
+            lenn = float(self.lineEdiLen.text())
+        except:
             QtWidgets.QMessageBox.critical(self, "", 
-                    "Угол поворота и длина должны быть целыми числами!")
-            return
+                    "Угол поворота и длина должны быть вещественные числами!")
+            return 
         if angle < 1:
             QtWidgets.QMessageBox.critical(self, "", "Угол поворота должен быть больше нуля!")
-            return
+            return 
         if lenn < 1:
             QtWidgets.QMessageBox.critical(self, "", "Длина отрезка должна быть больше нуля!")
-            return
+            return 
         teta = 0
         h = self.graphicsView.height()
         w = self.graphicsView.width()
         while teta < 2 * pi - 0.001:
-            # print("TETA", teta, 2 * pi - 0.001 )
             x = w // 2 + lenn * cos(teta)
             y = h // 2 + lenn * sin(teta)
-            
             self.my_addline([w // 2, h // 2], [x, y], draw=draw)
             teta += radians(angle)
+
 
     def draw_line(self):
         try:
@@ -173,8 +198,8 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
             y_start = float(self.lineEditStartY.text())
             y_end = float(self.lineEditEndY.text())
         except TypeError:
-            QtWidgets.QMessageBox.critical(self, "", "Координаты начала и конца отрезка должны быть целыми "
-                                                     "или веществеными числами!")
+            QtWidgets.QMessageBox.critical(self, "", "Координаты начала и конца отрезка должны быть "
+                                                     "веществеными числами!")
             return
         self.my_addline([x_start, y_start], [x_end, y_end])
             
@@ -194,8 +219,8 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
         s_x = sign(dx)
         s_y = sign(dy)
         # Вычисление модуля приращения.
-        dx = fabs(dx)
-        dy = fabs(dy)
+        dx = abs(dx)
+        dy = abs(dy)
         # Обмен местами координат в случае m > 1 (тангенс)
         if (dy >= dx):
             dx, dy = dy, dx
@@ -241,6 +266,7 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
         if steps:
             return step
     
+
     def bresen_int(self, p_start, p_end, draw=True, steps=False):
         # Проверка на вырожденность отрезка в точку.
         if p_start[0] == p_end[0] and p_start[1] == p_end[1]:
@@ -256,8 +282,8 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
         s_x = sign(dx)
         s_y = sign(dy)
         # Вычисление модуля приращения.
-        dx = fabs(dx)
-        dy = fabs(dy)
+        dx = abs(dx)
+        dy = abs(dy)
         # Обмен местами координат в случае m > 1 (тангенс)
         if (dy >= dx):
             dx, dy = dy, dx
@@ -266,6 +292,7 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
             chenge = 0
         # Иницилизация начального значения ошибки.
         m = 2 * dy # в случае FLOAT m = dy / dx
+        m1 = 2 * dx
         e = m - dx # в случае FLOAT e = m - 0.5
         # Инициализации начальных значений текущего пикселя 
         # (т е начало отрезка тут)
@@ -285,7 +312,7 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
                     x += s_x
                 else:
                     y += s_y
-                e -= 2 * dx # в случае FLOAT e -= 1
+                e -= m1 # в случае FLOAT e -= 1
             if e <= 0:
                 if chenge == 1:
                     y += s_y
@@ -305,7 +332,6 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
 
     def bresen_smooth(self, p_start, p_end, draw=True, steps=False):
         I = 255
-        # print("draw in draw", draw)
         # Проверка на вырожденность отрезка в точку.
         if p_start[0] == p_end[0] and p_start[1] == p_end[1]:
             if draw:
@@ -320,8 +346,8 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
         s_x = sign(dx)
         s_y = sign(dy)
         # Вычисление модуля приращения.
-        dx = fabs(dx)
-        dy = fabs(dy)
+        dx = abs(dx)
+        dy = abs(dy)
         # Обмен местами координат в случае m > 1 (тангенс)
         if (dy >= dx):
             dx, dy = dy, dx
@@ -359,7 +385,6 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
                 y += s_y
                 e -= W
             if draw:
-                # print("DDDD")
                 self.draw_point(x, y, round(e))
             if steps:
                 if not((x_buf == x and y_buf != y) or
@@ -373,9 +398,6 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
 
         
     def dda(self, p_start, p_end, draw=True, steps=False):
-        '''
-
-        '''
         # Проверка на вырожденность отрезка в точку.
         if p_start[0] == p_end[0] and p_start[1] == p_end[1]:
             if draw:
@@ -383,14 +405,12 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
                 QtWidgets.QMessageBox.critical(self, "Информация", 
                 "Внимание! Нарисована будет только точка.")
             return
-        # print(p_start, p_end)
         dx = p_end[0] - p_start[0]
         dy = p_end[1] - p_start[1]
-        # print(dx, dy)
-        if fabs(dx) > fabs(dy):
-            L = fabs(dx)
+        if abs(dx) > abs(dy):
+            L = abs(dx)
         else:
-            L = fabs(dy)
+            L = abs(dy)
 
         sx = (p_end[0] - p_start[0]) / L
         sy = (p_end[1] - p_start[1]) / L
@@ -400,18 +420,14 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
         i = 1
         step = 1
         while i <= L + 1:
-            # print("LOOP, i, L + 1", i, L + 1)
             if draw:
                  self.draw_point(round(x), round(y))
-            # print(x, x+dx, y, y+dy)
             if steps and i <= L:
                 if not((round(x + sx) == round(x) and 
                         round(y + sy) != round(y)) or 
                         (round(x + sx) != round(x) and 
                         round(y + sy) == round(y))):
-                    # print(step)
                     step += 1
-            
             x += sx
             y += sy
             i += 1
@@ -420,6 +436,7 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
     
 
     def wu(self, p_start, p_end, draw=True, steps=False):
+        Imax = 255
         if p_start[0] == p_end[0] and p_start[1] == p_end[1]:
             if draw:
                 self.draw_point(round(p_start[0]), round(p_start[1]))
@@ -428,18 +445,18 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
             return
         dx = p_end[0] - p_start[0]
         dy = p_end[1] - p_start[1]
-        if dy - dx < 0.1:
-            print()
-        Imax = 255
         m = 1
+        shag = 1
         step = 1
         if fabs(dy) > fabs(dx):
-            if p_start[1] > p_end[1]:
-                p_start[0], p_end[0] = p_end[0], p_start[0]
-                p_start[1], p_end[1] = p_end[1], p_start[1]
             if dy != 0:
                 m = dx / dy
-            for y in range(round(p_start[1]), round(p_end[1]) + 1):
+            m1 = m
+            if p_start[1] > p_end[1]:
+                m1 *= -1
+                shag *= -1
+            # print(shag, p_start[1], p_end[1], m1)
+            for y in range(round(p_start[1]), round(p_end[1]) + 1, shag):
                 d1 = p_start[0] - floor(p_start[0])
                 d2 = 1 - d1
                 if draw:
@@ -450,27 +467,27 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
                 if steps and y < round(p_end[1]):
                     if int(p_start[0]) != int(p_start[0] + m):
                         step += 1
-                p_start[0] += m
+                p_start[0] += m1
         else:
-            if p_start[0] > p_end[0]:
-                p_start[0], p_end[0] = p_end[0], p_start[0]
-                p_start[1], p_end[1] = p_end[1], p_start[1]
             if dx != 0:
                 m = dy / dx
-            for x in range(round(p_start[0]), round(p_end[0]) + 1):
+            m1 = m
+            if p_start[0] > p_end[0]:
+                shag *= -1
+                m1 *= -1
+            # print(shag, p_start[0], p_end[0], m1)
+            for x in range(round(p_start[0]), round(p_end[0]) + 1, shag):
                 d1 = p_start[1] - floor(p_start[1])
                 d2 = 1 - d1
                 if draw:
-                    pass
                     # нижняя точка
                     self.draw_point(x, int(p_start[1]), round(fabs(d2) * Imax))
                     # верхняя точка
                     self.draw_point(x, int(p_start[1]) + 1, round(fabs(d1) * Imax))
-
                 if steps and x < round(p_end[0]):
                     if int(p_start[1]) != int(p_start[1] + m):
                         step += 1
-                p_start[1] += m
+                p_start[1] += m1
         if steps:
             return step
 
@@ -482,7 +499,7 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
             return
         self.scene.addLine(round(p_start[0]), round(p_start[1]), 
                            round(p_end[0]), round(p_end[1]), self.pen)
-
+    
     def com_time(self):
        
         angle = int(self.lineEditAngle.text())
@@ -491,45 +508,41 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
 
         self.radioButtonCDA.toggle()
         start = time()
-        for _ in range(10):
+        for _ in range(100):
             self.draw_spectr(draw=False)
-            # cda_test([0,0], [200,200])
         time_array.append(time() - start)
 
         self.radioButtonBresFloat.toggle()
         start = time()
-        for _ in range(10):
-            # float_test([0,0], [200,200])
+        for _ in range(100):
             self.draw_spectr(draw=False)
         time_array.append(time() - start)
 
         self.radioButtonBresInt.toggle()
         start = time()
-        for _ in range(10):
-            # int_test([0,0], [200,200])
+        for _ in range(100):
             self.draw_spectr(draw=False)
         time_array.append(time() - start)
         
         self.radioButtonBresSmooth.toggle()
         start = time()
-        for _ in range(10):
-            # smoth_test([0,0], [200,200])
+        for _ in range(100):
             self.draw_spectr(draw=False)
         time_array.append(time() - start)
 
         self.radioButtonWu.toggle()
         start = time()
-        for _ in range(10):
+        for _ in range(100):
             self.draw_spectr(draw=False)
         time_array.append(time() - start)
         
         self.radioButtonLib.toggle()
         start = time()
-        for _ in range(10):
+        for _ in range(100):
             self.draw_spectr(draw=False)
         time_array.append(time() - start)
 
-        plt.figure(figsize=(25, 10))
+        plt.figure(figsize=(10, 5))
         plt.rcParams['font.size'] = '14'
 
         plt.bar(["ЦДА", "Брезенхем\n(float)", "Брезенхем\n(int)", 
@@ -542,18 +555,11 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
 
     def com_step(self):
         try:
-            angle = int(self.lineEditAngle.text())
-            lenn = int(self.lineEdiLen.text())
-        except TypeError:
-            QtWidgets.QMessageBox.critical(self, "", 
-                    "Угол поворота и длина должны быть целыми числами!")
-            return
-        if angle < 1:
-            QtWidgets.QMessageBox.critical(self, "", "Угол поворота должен быть больше нуля!")
-            return
-        if lenn < 1:
-            QtWidgets.QMessageBox.critical(self, "", "Длина отрезка должна быть больше нуля!")
-            return
+            angle = float(self.lineEditAngle.text())
+            lenn = float(self.lineEdiLen.text())
+        except:
+            lenn = 10
+            angle = 5
         h = self.graphicsView.height()
         w = self.graphicsView.width()
         cda = []
@@ -564,7 +570,6 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
 
         teta = 0
         angle = [i for i in range(0, 91, 2)]
-        # print(angle)
         while teta <= pi / 2 + 0.01:
             x = w // 2 + lenn * cos(teta)
             y = h // 2 + lenn * sin(teta)
@@ -575,7 +580,7 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
             wu.append(self.wu([w //2, h // 2], [x, y], False, True))
             teta += radians(2)
 
-        plt.figure(figsize=(25, 10))
+        plt.figure(figsize=(12, 8))
         plt.rcParams['font.size'] = '14'
         plt.plot(angle, cda, label='ЦДА')
         plt.plot(angle, b_float, label='Брезенхем\n(float/int)')
@@ -586,25 +591,15 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
         plt.ylabel("Максимальное колличество ступенек.")
         plt.xlabel("Угол в градусах.")
         plt.show()
-        # QtWidgets.QMessageBox.information(self, "Информация", 
-        #             "Исследование ступенчатости отрезков\n\n"
-        #             "%5.2f - длина отрезка\n"
-        #             .format(lenn))
+
 
     def com_step_one(self):
         try:
-            angle = int(self.lineEditAngle.text())
-            lenn = int(self.lineEdiLen.text())
-        except TypeError:
-            QtWidgets.QMessageBox.critical(self, "", 
-                    "Угол поворота и длина должны быть целыми числами!")
-            return
-        if angle < 1:
-            QtWidgets.QMessageBox.critical(self, "", "Угол поворота должен быть больше нуля!")
-            return
-        if lenn < 1:
-            QtWidgets.QMessageBox.critical(self, "", "Длина отрезка должна быть больше нуля!")
-            return
+            angle = float(self.lineEditAngle.text())
+            lenn = float(self.lineEdiLen.text())
+        except:
+            lenn = 10
+            angle = 5
         h = self.graphicsView.height()
         w = self.graphicsView.width()
         cda = []
@@ -615,7 +610,6 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
 
         teta = 0
         angle = [i for i in range(0, 91, 2)]
-        # print(angle)
         while teta <= pi / 2 + 0.01:
             x = w // 2 + lenn * cos(teta)
             y = h // 2 + lenn * sin(teta)
@@ -633,7 +627,7 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
                 return
             teta += radians(2)
 
-        plt.figure(figsize=(25, 10))
+        plt.figure(figsize=(12, 8))
         plt.rcParams['font.size'] = '14'
         if (self.radioButtonCDA.isChecked()):
             plt.plot(angle, cda, label='ЦДА')
@@ -645,20 +639,12 @@ class Visual(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
         elif (self.radioButtonWu.isChecked()):
             plt.plot(angle, wu, label='Ву')
         
-        # ax.set_facecolor('seashell')
         plt.title("Исследование ступенчатости.\n{0} - длина отрезка".format(lenn))
         plt.legend()
         plt.ylabel("Максимальное колличество ступенек.")
         plt.xlabel("Угол в градусах.")
         plt.show()
 
-
-def sign(x):
-    if x < 0:
-        return -1
-    elif x > 0:
-        return 1
-    return 0
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
